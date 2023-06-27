16e72fac16da4e2523660740ca51aa55a85311b8
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-27 15:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+// Get the required HTML elements
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const outputDiv = document.getElementById("output");
+const btn = document.getElementById("btn");
+
+// Function to display the message on the webpage after a delay
+async function displayMessage() {
+  const message = textInput.value;
+  const delay = parseInt(delayInput.value);
+
+  if (!message || isNaN(delay)) {
+    outputDiv.innerText = "Please enter a valid message and delay.";
+    return;
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, delay));
+  outputDiv.innerText = message;
+}
+
+// Event listener for the button click
+btn.addEventListener("click", displayMessage);
+
